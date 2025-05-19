8d87f8c22dc3a4b3246f3aa22ad8f559efe4bf29
ChristianBingman <christianbingman@gmail.com>
2025-05-19 18:26:21 -0500

    Make account listing look nicer and create basic model for ledger

--- ledger/models.py
+++ ledger/models.py
@@ -1,1 +1,17 @@
-# Create your models here.
+from django.db import models
+from datetime import datetime
+from acctmgr.models import Account, Currency
+
+class TransactionDetail(models.Model):
+    id = models.BigAutoField("transaction id", primary_key=True)
+    description = models.CharField(max_length=100)
+    xact_date = models.DateField(default=datetime.now)
+
+class Transaction(models.Model):
+    # Deleting the transaction detail should delete all entries for that xact
+    transaction_id = models.ForeignKey(TransactionDetail, on_delete=models.CASCADE)
+    # Deleting the account without explicitly deleting all transactions is restricted
+    account = models.ForeignKey(Account, on_delete=models.RESTRICT)
+    memo = models.CharField(max_length=256)
+    price = models.DecimalField(decimal_places=10, max_digits=19, default=1.0)
+    amount = models.DecimalField(decimal_places=10, max_digits=19)
--- ledger/tests.py
+++ ledger/tests.py
@@ -1,1 +1,29 @@
-# Create your tests here.
+import pytest
+
+def test_create_transaction():
+    # A double and many entry transaction should create
+    ...
+
+def test_create_transaction_with_invalid_decimal():
+    # Amounts that don't abide by the fraction traded should fail
+    ...
+
+def test_invalid_transaction_amount():
+    # All transactions should equal 0 in double entry
+    ...
+
+def test_default_transaction_values():
+    # Test the default values for transaction and transaction details
+    ...
+
+def test_account_deletion_restricted():
+    # Deleting an account without also deleting the entries should be restricted
+    ...
+
+def test_account_transaction_query():
+    # Test querying all transactions by account
+    ...
+
+def test_query_entries_by_transaction():
+    # Test querying all entries related to transaction detail
+    ...
